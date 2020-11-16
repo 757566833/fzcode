d6b68c3123e71ceb11236bd0c45e66bb960358f1
赵宏健 <757566833@qq.com>
2020-11-16 10:43:04 +0800

    开始改造gate 留个commit方便回退

--- gate-service/src/main/java/com/fzcode/gateservice/controller/TestController.java
+++ gate-service/src/main/java/com/fzcode/gateservice/controller/TestController.java
@@ -1,21 +1,29 @@
-//package com.examination.gateservice.controller;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import reactor.core.publisher.Mono;
-//
-//import java.security.Principal;
-//import java.util.Collections;
-//import java.util.Map;
-//
-//@RestController
-//public class TestController {
-//    @GetMapping("/")
-//    public Mono<Map<String,String>> test(Mono<Principal> principalMono){
-//        return principalMono.map(Principal::getName).map(this::test2);
-//    }
-//
-//    public Map<String,String> test2 (String username){
-//        return Collections.singletonMap("message","hello "+ username);
-//    }
-//}
+package com.fzcode.gateservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class TestController {
+    private String key;
+
+    @Value("${auth.secret}")
+    public void setKey(String secret) {
+        this.key = secret;
+    }
+
+    @GetMapping("/current")
+    public Mono<String> test(Mono<Principal> principalMono) {
+        return Mono.just(this.key);
+    }
+
+    public Map<String, String> test2(String username) {
+        return Collections.singletonMap("message", "hello " + username);
+    }
+}
