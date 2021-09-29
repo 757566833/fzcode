d11870ad908104bc15fd0a43d3c32d58e0208ce4
赵宏健 <757566833@qq.com>
2021-09-29 17:27:08 +0800

    fixed 清除无效引入

--- cloud-gate/src/main/java/com/fzcode/cloudgate/CloudGateApplication.java
+++ cloud-gate/src/main/java/com/fzcode/cloudgate/CloudGateApplication.java
@@ -1,11 +1,9 @@
 package com.fzcode.cloudgate;
 
 import com.fzcode.cloudgate.config.Services;
-import com.fzcode.internalcommon.constant.ServiceNameConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
