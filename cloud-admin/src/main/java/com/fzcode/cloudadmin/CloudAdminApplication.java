d0289467b860e9993fae8583b0f3bd01146505d1
赵宏健 <757566833@qq.com>
2021-10-19 11:31:36 +0800

    feature 添加admin服务

---
+++ cloud-admin/src/main/java/com/fzcode/cloudadmin/CloudAdminApplication.java
@@ -0,0 +1,20 @@
+package com.fzcode.cloudadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@EnableAdminServer
+@SpringBootApplication
+public class CloudAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAdminApplication.class, args);
+    }
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+    }
+}
